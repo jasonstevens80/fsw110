d271ccaceb252ffc0f74cd33aecb0d5e310750f6
jasonstevens80 <71710082+jasonstevens80@users.noreply.github.com>
2020-11-24 17:48:54 -0500

    Add files via upload

---
+++ week2/index.js
@@ -0,0 +1,29 @@
+var newh1 = document.createElement("h1")
+newh1.textContent = "Welcome to my JS site"
+document.body.append(newh1)
+newh1.style.textAlign ="center"
+newh1.style.fontsize = "30px"
+
+var newLi = document.createElement("li")
+newLi.textContent =("Look how crazy Javascript is")
+newLi.style.fontsize = "12px"
+document.body.append(newLi)
+
+var newLi = document.createElement("li")
+newLi.textContent =("It's crazy how it all works")
+newLi.style.fontsize = "12px"
+document.body.append(newLi)
+
+var newLi = document.createElement("li")
+newLi.textContent =("Im exciting to put it all together")
+newLi.style.fontsize = "12px"
+document.body.append(newLi)
+
+var newP = document.createElement("P")
+newP.textContent = "All of this was created using Javasciprt"
+document.body.append(newP)
+newP.style.textAlign ="center"
+
+
+
+
